25d4a13e2d1e5e4b32e5e093f41a128e8edcf565
Clément Théraud <theraud.clement@gmail.com>
2022-01-20 13:19:08 +0100

    F sort by

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,11 +1,17 @@
 //PAGE PHOTOGRAPHER
 //Dropdown Filter
-function sortBy(photograperMedias) {
+function sortBy(photographerMedias) {
     const option = document.querySelector(#sort-by).value;
 
     if (option == "popularity") {
         photographerMedias.sort(function (a, b) {
             return b.likes - a.likes;
             });
+    } else if (option == date) {
+        photographerMedias.sort (function (a, b) {
+            let dateA = new Date (a.date),
+            dateB = new Date (b.date);
+            return dateA - dateB;
+        });
     }
 }
