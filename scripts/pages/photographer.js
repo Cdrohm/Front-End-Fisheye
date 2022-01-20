eacb62fb4576805e65afc8bbeead63f2026cf220
Clément Théraud <theraud.clement@gmail.com>
2022-01-20 13:16:10 +0100

    test sort by

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,11 @@
-//Mettre le code JavaScript lié à la page photographer.html
+//PAGE PHOTOGRAPHER
+//Dropdown Filter
+function sortBy(photograperMedias) {
+    const option = document.querySelector(#sort-by).value;
+
+    if (option == "popularity") {
+        photographerMedias.sort(function (a, b) {
+            return b.likes - a.likes;
+            });
+    }
+}
