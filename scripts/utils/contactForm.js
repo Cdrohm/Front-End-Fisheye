b00a14780e31da33102e0c63647ab79813fce94e
Clément Théraud <theraud.clement@gmail.com>
2022-01-24 09:36:17 +0100

    form modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,16 @@ function closeModal() {
   const modal = document.getElementById("contact_modal");
   modal.style.display = "none";
 }
+
+//FORM MODAL
+/*function formModal(photographer) {
+  const photographerNameContent = document.querySelector("#form-modal div.form-text");
+  const photographerName = document.createElement("span");
+  photographerName.textContent = photographer.name;
+  photographerNameContent.appendChild(photographerName);
+
+  const formModalBg = document.querySelector("#form-modal.bground");
+  const closeFormBtn = document.querySelector("#form-modal button.close");
+  const formModalBtn = document.querySelector("#main-photographer #contact");
+
+  */
